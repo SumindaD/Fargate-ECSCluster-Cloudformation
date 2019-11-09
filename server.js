df2f86086c671d6e8bdf9354021249d9c9273534
Suminda Niroshan <sumindaniro@gmail.com>
2019-11-10 03:07:21 +0530

    Updated to version 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Version 1');
+  res.send('CI/CD is Working!');
 });
 
 app.listen(PORT, HOST);
